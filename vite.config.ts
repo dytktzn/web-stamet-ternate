1a90640122c91552985131565dc05d8d3dba96ce
dytktzn <hidayatullah4blog@gmail.com>
2026-04-13 15:05:05 +0900

    Set base URL for GitHub Pages
    Added base URL configuration for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/web-stamet-ternate/", // Ini penambahan base url untuk GitHub Pages
   server: {
     host: "::",
     port: 8080,
